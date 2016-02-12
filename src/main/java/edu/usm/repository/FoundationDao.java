a3ed13ffefcd41885126a44ec20a79441400a475
Andrew Taylor <andrew.dorman.taylor@gmail.com>
2016-02-12 14:09:25 -0500

    Override findAll of CrudRepository to return a HashSet

--- src/main/java/edu/usm/repository/FoundationDao.java
+++ src/main/java/edu/usm/repository/FoundationDao.java
@@ -3,8 +3,14 @@ package edu.usm.repository;
 import edu.usm.domain.Foundation;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.HashSet;
+
 /**
  * Created by andrew on 1/24/16.
  */
 public interface FoundationDao extends CrudRepository<Foundation, String> {
+
+    @Override
+    HashSet<Foundation> findAll();
+
 }
