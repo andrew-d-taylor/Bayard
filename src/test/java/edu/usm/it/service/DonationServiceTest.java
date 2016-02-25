a2353b24ce73a22d63e5f4b2f088334ddc9d9825
Andrew Taylor <andrew.dorman.taylor@gmail.com>
2016-02-25 12:29:07 -0500

    Very basic donationservice test

--- src/test/java/edu/usm/it/service/DonationServiceTest.java
+++ src/test/java/edu/usm/it/service/DonationServiceTest.java
@@ -9,6 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 /**
  * Created by andrew on 2/25/16.
  */
@@ -28,6 +32,8 @@ public class DonationServiceTest extends WebAppConfigurationAware {
         donation.setDateOfDeposit(LocalDate.of(2015, 12, 12));
         donation.setDateOfReceipt(LocalDate.of(2015, 11, 11));
         donation.setMethod("Credit Card");
+        donation.setRestrictedToCategory("Office supplies");
+        donation.setStandalone(true);
     }
 
     @Autowired
@@ -37,18 +43,31 @@ public class DonationServiceTest extends WebAppConfigurationAware {
 
     @Test
     public void testCreateDonation() {
-
+        donationService.create(donation);
+        donation = donationService.findById(donation.getId());
+        assertNotNull(donation);
     }
 
     @Test
     public void testUpdateDonation() {
+        donationService.create(donation);
+        int newDonationAmount = donation.getAmount() + 10;
+        donation.setAmount(newDonationAmount);
+        donationService.update(donation);
 
+        donation = donationService.findById(donation.getId());
+        assertEquals(newDonationAmount, donation.getAmount());
     }
 
     @Test
     public void testDeleteDonation() {
+        donationService.create(donation);
+        donation = donationService.findById(donation.getId());
 
+        donationService.delete(donation);
+        donation = donationService.findById(donation.getId());
 
+        assertNull(donation);
     }
 
 }
